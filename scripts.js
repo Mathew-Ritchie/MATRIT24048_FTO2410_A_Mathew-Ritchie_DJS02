faa522d53245a5fd60e07d6f0f5d7cdb06799560
Mathew Ritchie <ritchmat89@gmail.com>
2025-03-11 19:46:54 +0200

    scenario 3 and 4 completed using try/catch

--- scripts.js
+++ scripts.js
@@ -5,8 +5,33 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+
+  // Scenario 2 - Both inputs fields need an input.
   if (!dividend || !divider) {
     return (result.innerText = `Division not performed. Both values are required in inputs. Try again`);
   }
+
+  // Scenario 3 - divider cannot be zero and should throw an error.
+  if (parseInt(divider) === 0) {
+    try {
+      throw new Error("Division by zero not possible");
+    } catch (error) {
+      console.error("Error during division:", error.stack);
+      result.innerText = `Division not performed. Invalid number provided. Try again`;
+      return;
+    }
+  }
+  // Scenario 3 - use of 'YOLO' and '+++' on submition should make entire screen be replaced by message and log the call stack to the console.
+  if (dividend === "YOLO" && divider === "+++") {
+    try {
+      throw new Error("Critical Error!");
+    } catch (error) {
+      console.error("Wrong inputs used", error.stack);
+      document.body.innerHTML = `Something critical went wrong. Please reload the page`;
+      return;
+    }
+  }
+
+  // Scenario 1 - removing decimals
   result.innerText = Math.trunc(dividend / divider);
 });
