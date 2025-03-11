a532c7167b584a337844e550474d612684a691ff
Mathew Ritchie <ritchmat89@gmail.com>
2025-03-11 19:15:46 +0200

    completed second user story requirement. now both the dividene and divider require a value, if a value is missing then the code will return a message in the results html

--- scripts.js
+++ scripts.js
@@ -5,5 +5,8 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  if (!dividend || !divider) {
+    return (result.innerText = `Division not performed. Both values are required in inputs. Try again`);
+  }
   result.innerText = Math.trunc(dividend / divider);
 });
